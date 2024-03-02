79227d68f49d3e4286935a63d10947a509371f4e
ahmedWanneni <ahmed.wanneni@gmail.com>
2024-03-02 13:59:45 +0100

    feat: change build entry

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
          external: ['react', 'react/jsx-runtime'],
+          treeshake: true,
+          output: {
+            entryFileNames: `index.js`,
+            chunkFileNames: `index-chunk.js`,
+            assetFileNames: `[name].[ext]`,
+          }
     },
     copyPublicDir: false,
      lib: {
